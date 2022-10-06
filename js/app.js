fce5f836f9af09ba22df89392f963cd7a79ac990
inflictx <inflictx@gmail.com>
2022-10-06 15:27:52 +0300

    finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 window.onload = function () {
-  Draggable.create(".gallery", {
-    bounds: "body" /* ограничение скрола */,
-    inertia: true /* иннерция */,
-  });
+  setTimeout(function () {
+    document.body.classList.add("loaded");
+
+    Draggable.create(".gallery", {
+      bounds: "body" /* ограничение скрола */,
+      inertia: true /* иннерция */,
+    });
+  }, 200);
 };
